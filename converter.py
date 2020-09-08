a5ed15f43840a2b36961d405723bb68f3057b031
Calvin Tirrell <calvintirrell@gmail.com>
2020-09-07 17:52:00 -0700

    Add files via upload

--- converter.py
+++ converter.py
@@ -1,6 +1,4 @@
 # imported packages to create this script/program
-import os
-import pandas as pd
 from tkinter import *
 from tkinter import messagebox
 from docx2pdf import convert
@@ -15,32 +13,22 @@ class Window(Frame):
     # Creation of init_window
     def init_window(self):
 
-        # changing the title of our master widget
-        self.master.title("Word (.docx) To PDF Converter")
+        # changing the title of our widget
+        self.master.title("Word (.docx) To PDF")
 
         # allowing the widget to take the full space of the root window
         self.pack(fill=BOTH, expand=1)
 
-        # label describing the Run button
-        runLabel = Label(self, text="Click 'Run' to start")
-        runLabel.pack()
-        runLabel.place(x=100, y=125)
-
         # the Run button and it's attributes
-        runButton = Button(self, text="Run", command=self.conversion)
+        runButton = Button(self, text="RUN: Word (.docx) to PDF", command=self.docx_pdf_convert)
         runButton.place(x=50, y=125)
 
-        # label describing the Run button
-        quitLabel = Label(self, text="Click 'Quit' to close")
-        quitLabel.pack()
-        quitLabel.place(x=100, y=150)
-
-        # the Quit button and it's attributes
-        quitButton = Button(self, text="Quit", command=self.client_exit)
+        # the Quit button and it's attributes/function
+        quitButton = Button(self, text="Quit Program", command=self.client_exit)
         quitButton.place(x=50, y=150)
 
     # function to find and convert '.docx' files into PDFs
-    def conversion(self):
+    def docx_pdf_convert(self):
 
         # indicates current working directory (where this script is located)
         dir = '.'
